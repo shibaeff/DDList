f7f2103b15085c1631fe73cc9bcf6a279e79d5b2
shibaeff <shibaev.pp@phystech.edu>
2018-10-08 18:00:11 +0300

    Added first/last test for empty list

--- test_doublyLinkedList.py
+++ test_doublyLinkedList.py
@@ -6,3 +6,11 @@ class TestDoublyLinkedList(TestCase):
     def test_len_empty(self):
         list = DoublyLinkedList()
         self.assertEqual(list.len(), 0)
+
+    def test_empty_first_last(self):
+        list = DoublyLinkedList()
+        from Exceptions.exceptions import EmptyListOperationError
+        with self.assertRaises(EmptyListOperationError):
+            print(list.first())
+        with self.assertRaises(EmptyListOperationError):
+            print(list.last())
